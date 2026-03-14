f977968c0a8d0f7419b4f64dca2f7bd0d857d49f
shadow0p-295 <ayushbajeli669@gmail.com>
2026-03-14 17:56:16 +0000

    datatypes and variables

---
+++ 01basics/datatypes.js
@@ -0,0 +1,9 @@
+"use strict"; //treat all js code as newer version
+alert(5+5)  // we are using nodejs not browser
+let a=null
+let b;
+console.log(typeof null)   // object
+console.log(typeof undefined)  //undefined
+
+//symbol - unique
+// 
---
+++ 01basics/variables.js
@@ -0,0 +1,13 @@
+const accountid=12345
+let accountemail="ayushbajeli3333@gmail.com"
+var accountpass="23455"
+accountcity="tehri"
+let accountstate;
+
+//accountid=445466
+console.table([accountid,accountemail,accountpass,accountcity,accountstate])
+
+/*
+
+prefer not to use var beacause of issue in block scope and fuctional scope
+*/

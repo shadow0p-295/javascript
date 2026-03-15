1fc9ec110351a0b0288d68dd9332532395c0eeec
shadow0p-295 <ayushbajeli669@gmail.com>
2026-03-15 05:25:22 +0000

    conversions of datatypes

---
+++ 01basics/conversion.js
@@ -0,0 +1,21 @@
+let score="334abc"
+let valueinnumber=Number(score)
+let score1;
+let valuenumber1=Number(score1)
+//console.log(typeof valuenumber1)
+//console.log(valuenumber1)
+
+let ab=1
+let booleanofab=Boolean(ab)
+
+let num= 33
+let strofnum = String(num)
+console.log(strofnum)
+console.log(typeof strofnum)
+
+
+ //console.log(booleanofab)
+// "33" =  33
+// "33abc" = NaN
+//true = 1     false=0
+// "" = false     "abc" = true
